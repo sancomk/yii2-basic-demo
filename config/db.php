9770302b340c60babafeec94cd9328f197a88621
sancomk <sanco.mk@gmail.com>
2017-04-23 20:11:18 +0300

    add config/db.php

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=yii2demo',
+    'username' => 'yii2basicdemo',
+    'password' => 'yii2basicdemo',
+    'charset' => 'utf8',
+];
